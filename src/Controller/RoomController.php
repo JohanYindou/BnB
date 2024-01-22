d190087bd0cf650e9c178d992b2e11e46331d47d
Johan Yindou <jyindou@gmail.com>
2024-01-22 11:25:19 +0100

    Parametrage de la pagination

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 #[Route('/r')]
@@ -13,11 +15,18 @@ class RoomController extends AbstractController
 {
     #[Route('/', name: 'app_room')]
     public function index(
-        RoomRepository $roomRepository
+        RoomRepository $roomRepository,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response
     {
+        $pagination = $paginator->paginate(
+            $roomRepository->findAll(), // All rooms
+            $request->query->getInt('page', 1), // Check page number
+            12 /*limit per page*/
+        );
         return $this->render('room/index.html.twig', [
-            'rooms' => $roomRepository->findAll(),
+            'rooms' => $pagination,
             'hostRooms' => $roomRepository->findBy(
                 ['host' => $this->getUser()]
                 )
