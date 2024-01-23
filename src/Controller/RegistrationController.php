b650a3cbe67443650dc285dea4f9470fe4ff6895
Johan Yindou <jyindou@gmail.com>
2024-01-23 17:05:59 +0100

    Traitement du formulaire de complétion du profil

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,16 +47,22 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@bnb.fr', 'BnB Team'))
+                    ->from(new Address('tot@bnb.fr', 'Toto de BnB'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('paris');
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
         }
 
         return $this->render('registration/register.html.twig', [
@@ -81,6 +87,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('paris');
+        return $this->redirectToRoute('account');
     }
 }
