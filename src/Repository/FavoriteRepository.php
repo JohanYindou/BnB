61a2fcfbe2e74801bcb2320913ce1203563bb69f
Johan Yindou <jyindou@gmail.com>
2024-01-24 10:23:19 +0100

    🔧 corriger(FavoriteRepository.php) : importer les classes manquantes (Room, User)
    ✨ fonctionnalité(FavoriteRepository.php) : ajouter une méthode pour vérifier si une chambre est favorisée par un utilisateur

--- src/Repository/FavoriteRepository.php
+++ src/Repository/FavoriteRepository.php
@@ -2,9 +2,11 @@
 
 namespace App\Repository;
 
+use App\Entity\Room;
+use App\Entity\User;
 use App\Entity\Favorite;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Favorite>
@@ -21,28 +23,39 @@ class FavoriteRepository extends ServiceEntityRepository
         parent::__construct($registry, Favorite::class);
     }
 
-//    /**
-//     * @return Favorite[] Returns an array of Favorite objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('f.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function isRoomFavorite(User $user, Room $room): bool
+    {
+        return $this->createQueryBuilder('f') // f for favorite
+            ->andWhere('f.traveler = :user') // f.traveler is the user who favorited the room
+            ->andWhere(':room MEMBER OF f.rooms') // f.rooms is the collection of rooms favorited by the user
+            ->setParameter('user', $user) // :user is the user who is logged in
+            ->setParameter('room', $room) // :room is the room we want to check if it's favorited by the user
+            ->getQuery() // get the query
+            ->getOneOrNullResult() !== null; // if the query returns a result, it means the room is favorited by the user
+    }
 
-//    public function findOneBySomeField($value): ?Favorite
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Favorite[] Returns an array of Favorite objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('f')
+    //            ->andWhere('f.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('f.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Favorite
+    //    {
+    //        return $this->createQueryBuilder('f')
+    //            ->andWhere('f.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
